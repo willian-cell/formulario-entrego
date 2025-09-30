2a1718b13c53b86f8eb39868e5226395fbcd7598
EFG <efg@efg.local>
2025-09-30 20:38:37 -0300

    das

--- wsgi.py
+++
@@ -1,9 +0,0 @@
-import sys
-import os
-
-# Caminho do seu projeto no PythonAnywhere
-path = '/home/Willian96w38528736/mysite'
-if path not in sys.path:
-    sys.path.insert(0, path)
-
-from app import app as application
